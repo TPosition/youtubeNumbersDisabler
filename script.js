641008ebd93585c3d323ddc5745a6e560c16f09a
TPosition <45853874+TPosition@users.noreply.github.com>
2021-06-09 07:23:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,12 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @match        http://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-console.log("content.js");
-
 const disabledKeyCodes = new Set([
-  12,
-  33,
-  34,
   35,
   36,
-  37,
-  38,
-  39,
-  40,
   45,
   48,49,50,51,52,53,54,55,56,57,58,59,
   96,
@@ -36,8 +26,10 @@ const disabledKeyCodes = new Set([
 window.addEventListener(
   "keydown",
   function(e) {
+      console.log(e);
     if (disabledKeyCodes.has(e.which)) {
       e.stopPropagation();
+
     }
   },
   true
